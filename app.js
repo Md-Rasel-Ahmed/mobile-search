aa2fcf2dd48897a09345a8635a18b1185bba54da
Rasel-ahemd <nhd305812@gmail.com>
2022-03-01 01:42:21 +0500

    loaded success

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 // All dom selector
 let form = document.querySelector("form");
 let inputField = document.getElementById("input-field");
+let phoneName = document.getElementById("phone-name");
+let phoneImg = document.getElementById("phone-img");
+let row = document.querySelector(".row");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("done");
+  loadData();
 });
+const loadData = () => {
+  fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${inputField.value}`
+  )
+    .then((res) => res.json())
+    .then((data) => displayData(data.data));
+};
+// loadData();
+const displayData = (data) => {
+  // console.log(data);
+  data.forEach((phone) => {
+    console.log(phone.brand);
+    let div = document.createElement("div");
+  });
+};
